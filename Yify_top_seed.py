1003d511a44a8b240495ac9e89a1c728d964c1f8
nateshmbhat <nateshmbhat1@gmail.com>
2017-11-05 22:53:56 +0530

    gets the top seeded names with removed 1080p names and duplicates

--- Yify_top_seed.py
+++ Yify_top_seed.py
@@ -3,15 +3,27 @@ import bs4
 import time
 import urllib3
 import smssender
+import json
 
 class yifyer:
 	def __init__(self):
 		resp = requests.get('https://www.yify-torrent.org/') ;
-		soup = bs4.BeautifulSoup(resp.text , 'html.parser') ;
-		self.topseeds = soup.find(id='topseed').find_all('a') ;
-		print(self.topseeds) ;
+		self.soup = bs4.BeautifulSoup(resp.text , 'html.parser') ;
 
+	def find_top_seeded_movies(self):
+		self.topseeds = self.soup.find(id='topseed').find_all('a') ;
+		topseeds = [] ;
+		
+		for i in self.topseeds:
+			topseeds.append(i.text) ;
+
+		for i in range(len(topseeds)):
+			topseeds[i] = topseeds[i].strip('1080p').strip() ; 
+
+		topseeds = list(set(topseeds)) ;
+		
 
 
 if(__name__=='__main__'):
 	yify = yifyer() ;
+	yify.find_top_seeded_movies() ;
